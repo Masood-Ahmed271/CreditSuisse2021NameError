fe502c30c3756519feaa216e49fdfa001b56ac7d
Masood Ahmed <63158543+Masood-Ahmed271@users.noreply.github.com>
2021-09-26 02:22:18 +0800

    decoder logic 2

---
+++ codeitsuisse/routes/decoder.py
@@ -0,0 +1,16 @@
+import logging
+import random
+from flask import request, jsonify
+from codeitsuisse import app
+
+logger = logging.getLogger(_name_)
+
+@approute('/decoder', methods=['POST'])
+
+def evaluateDecoder():
+    info = request.get_data(as_text=True)
+    logging.info("data sent for evaluation {}".format(info))
+    possible = info['possible_values']
+    num = info['num_slots']
+    guess = random.sample(possible, num)
+    return guess

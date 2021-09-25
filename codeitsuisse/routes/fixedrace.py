6429d29571e7af17c7d86384210e097b86adef3c
abdulwadoodfaazli <83848781+abdulwadoodfaazli@users.noreply.github.com>
2021-09-26 00:30:24 +0800

    changed by AW

--- codeitsuisse/routes/fixedrace.py
+++ codeitsuisse/routes/fixedrace.py
@@ -10,22 +10,10 @@ logger = logging.getLogger(__name__)
 
 @app.route('/fixedrace', methods=['POST'])
 def evaluatefixedrace():
-    data = request.get_data()
+    info = request.get_data(as_text=True)
     logging.info("data sent for evaluation {}".format(data))
-    # inputValue = data.get("input")
-    result =""
-#     racers = data.split(",")
-#     result = fixedrace(racers)
-
-    return result
-
-    # logging.info("My result :{}".format(result))
-    # return jsonify(result)
-
-
-
-# def fixedrace(racers):
-# #     random.shuffle(racers)
-# #     return (','.join(racers))
-#     return ""
+    racers = info.split(',')
+    random.shuffle(racers)
+    rankings = ','.join(racers)
+    return rankings
     

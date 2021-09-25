8c847389e47f6bcd2307ed37e3e80adcbce7134e
Masood Ahmed <63158543+Masood-Ahmed271@users.noreply.github.com>
2021-09-25 19:37:11 +0800

    fixedrace logic by Abdulwadood

---
+++ fixedrace.py
@@ -0,0 +1,27 @@
+import logging
+import json
+import random
+
+from flask import request, jsonify
+
+from codeitsuisse import app
+
+logger = logging.getLogger(__name__)
+
+@app.route('/fixedrace', methods=['POST'])
+def evaluatefixedrace():
+    data = request.get_json()
+    logging.info("data sent for evaluation {}".format(data))
+    # inputValue = data.get("input")
+    result =""
+    racers = data.split(",")
+    result = fixedrace(racers)
+
+    return result
+
+
+
+def fixedrace(racers):
+    random.shuffle(racers)
+    return (','.join(racers))
+    

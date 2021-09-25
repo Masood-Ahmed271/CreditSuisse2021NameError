f450d7c31aef630e78b81ae903a7d09fd6968e19
Masood Ahmed <63158543+Masood-Ahmed271@users.noreply.github.com>
2021-09-26 00:36:32 +0800

    changed by aw again

--- codeitsuisse/routes/fixedrace.py
+++ codeitsuisse/routes/fixedrace.py
@@ -1,5 +1,4 @@
 import logging
-import json
 import random
 
 from flask import request, jsonify
@@ -10,22 +9,10 @@ logger = logging.getLogger(__name__)
 
 @app.route('/fixedrace', methods=['POST'])
 def evaluatefixedrace():
-    data = request.get_data()
+    data = request.get_data(as_text=True)
     logging.info("data sent for evaluation {}".format(data))
-    # inputValue = data.get("input")
-    result =""
-#     racers = data.split(",")
-#     result = fixedrace(racers)
-
+    racers = data.split(',')
+    random.shuffle(racers)
+    result = ','.join(racers)
     return result
-
-    # logging.info("My result :{}".format(result))
-    # return jsonify(result)
-
-
-
-# def fixedrace(racers):
-# #     random.shuffle(racers)
-# #     return (','.join(racers))
-#     return ""
     

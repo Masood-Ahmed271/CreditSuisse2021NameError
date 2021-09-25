7793c8bd799982ee273a25c28b2c423cebe036ec
Masood Ahmed <63158543+Masood-Ahmed271@users.noreply.github.com>
2021-09-26 02:21:37 +0800

    updated decoder

--- decoder.py
+++ decoder.py
@@ -7,8 +7,9 @@ logger = logging.getLogger(_name_)
 
 @approute('/decoder', methods=['POST'])
 
-def evaluateRace():
+def evaluateDecoder():
     info = request.get_data(as_text=True)
+    logging.info("data sent for evaluation {}".format(info))
     possible = info['possible_values']
     num = info['num_slots']
     guess = random.sample(possible, num)
